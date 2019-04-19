bbf44e99069af3b8cb9104fd072aa3091823e797
jainaaditya <42334537+jainaaditya@users.noreply.github.com>
2019-04-19 21:23:21 +0530

    Add files via upload

---
+++ middleware/middleware.js
@@ -0,0 +1,10 @@
+module.exports = {
+    checkUserAuth : function(req,res,next){
+        if(req.isAuthenticated()){
+            return next();
+        }
+        else{
+            res.redirect('/signin');
+        }
+    }
+}
